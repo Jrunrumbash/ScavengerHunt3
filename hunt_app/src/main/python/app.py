a2a27b31b0e94e562e5336117ede3c54c15fca70
Jrunrumbash <jrunswick@gmail.com>
2018-06-26 00:58:31 +0100

    Working login with database seeding.

--- hunt_app/src/main/python/app.py
+++ hunt_app/src/main/python/app.py
@@ -1,14 +1,11 @@
 import logging
-from hunt_app.src.main.python.hunt_database import SolvedClue
-from hunt_app.src.main.python.hunt_database import Team
-from hunt_app.src.main.python.hunt_database import Clue
-from hunt_app.src.main.python.hunt_database import HuntDatabase
-from hunt_app.src.main.python import auth
 
 from flask import Blueprint, Flask, render_template, redirect, request, url_for
 from flask_redis import FlaskRedis
 from redis import StrictRedis
 
+from hunt_app.src.main.python.auth import register_auth_module
+from hunt_app.src.main.python.hunt_database import start_database, seed_database
 
 stream_handler = logging.StreamHandler()
 stream_handler.setLevel(logging.INFO)
@@ -17,27 +14,20 @@ redis_store = FlaskRedis.from_custom_provider(StrictRedis)
 
 page = Blueprint('page', __name__)
 
-db = HuntDatabase()
+app = Flask(__name__, instance_relative_config=True)
+app.config.from_object('hunt_app.src.main.python.config.settings')
+app.config.from_pyfile('settings.py', silent=True)
+
 
 def create_app():
 
-    app = Flask(__name__, instance_relative_config=True)
-
-    app.config.from_object('hunt_app.src.main.python.config.settings')
-    app.config.from_pyfile('settings.py', silent=True)
-
     redis_store.init_app(app)
-    db.start_database(app)
-
     app.register_blueprint(page)
-    app.register_blueprint(auth.bp)
     app.logger.addHandler(stream_handler)
+    register_auth_module(app)
+    start_database(app)
 
     return app
-
-
-def getDatabase():
-    return db
 
 
 @page.route('/')
@@ -46,7 +36,7 @@ def index():
         feed_count = redis_store.incr('feed_count')
         main_content = 'Feeding time'
     elif request.args.get('test_message'):
-        main_content="Testing the main content insertion into the place."
+        main_content = "Testing the main content insertion into the place."
         feed_count = 0
     else:
         main_content = ''
@@ -59,7 +49,7 @@ def index():
 
 @page.route('/seed')
 def seed():
-    db.seed_database()
+    seed_database()
     return redirect(url_for('page.index'))
 
 
--- hunt_app/src/main/python/auth.py
+++ hunt_app/src/main/python/auth.py
@@ -4,10 +4,15 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 from werkzeug.security import check_password_hash, generate_password_hash
-from hunt_app.src.main.python import app
-from hunt_app.src.main.python.hunt_database import Team
+from hunt_app.src.main.python.hunt_database import Team, save_object, update_object
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+def register_auth_module(app):
+    print("Registering authorisation module")
+    app.register_blueprint(bp)
+
 
 @bp.route('/register', methods=('GET', 'POST'))
 def register():
@@ -15,7 +20,6 @@ def register():
         team_name = request.form['username']
         captain = request.form['captain']
         password = request.form['password']
-        db = app.getDatabase()
         error = None
 
         if not team_name:
@@ -29,12 +33,13 @@ def register():
 
         if error is None:
             new_team = Team(name=team_name, captain=captain, login_code=generate_password_hash(password))
-            db.save_object(new_team)
+            save_object(new_team)
             return redirect(url_for('auth.login'))
 
         flash(error)
 
     return render_template('auth/register.html')
+
 
 @bp.route('/login', methods=('GET', 'POST'))
 def login():
@@ -42,7 +47,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         error = None
-        team = Team.query.filter_by(name=username)
+        team = Team.query.filter_by(name=username).first()
 
         if team is None:
             error = 'Team name is incorrect'
@@ -52,11 +57,12 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = Team.id
-            return redirect(url_for('index'))
+            return redirect(url_for('page.index'))
 
         flash(error)
 
     return render_template('auth/login.html')
+
 
 @bp.before_app_request
 def load_logged_in_user():
@@ -67,10 +73,12 @@ def load_logged_in_user():
     else:
         g.user = Team.query.get(id=user_id)
 
+
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect(url_for('index'))
+    return redirect(url_for('page.index'))
+
 
 def login_required(view):
     @functools.wraps(view)
